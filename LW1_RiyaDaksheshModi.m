0b2a396ab0aedfd625478a5cc28f9d6c4f02b338
Riya Modi <riyamodi183@gmail.com>
2026-09-11 15:06:14 +0300

    Add files via upload

---
+++ LW1_RiyaDaksheshModi.m
@@ -0,0 +1,11 @@
+%Riya Dakshesh Modi, EDIfu25/1, 11/09/2026
+
+% Paprastas skriptas
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-0-]  |   F_2 [-x-]')
