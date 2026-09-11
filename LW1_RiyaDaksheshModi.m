0436cd3be65ed6faea55f87c56b8372875406166
Riya Modi <riyamodi183@gmail.com>
2026-09-11 15:22:52 +0300

    Completed complementary task

--- LW1_RiyaDaksheshModi.m
+++ LW1_RiyaDaksheshModi.m
@@ -9,3 +9,38 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-0-]  |   F_2 [-x-]')
+
+N = 5;
+
+% Vector
+v = N+1:0.5:N+4;
+
+% Matrix A
+A = [N   N+1 N+2;
+     N+3 N+4 N+5;
+     N+6 N+7 N+8];
+
+% Green element a
+a = A(3,2);
+
+% Green element b
+b = A(2:3,1:2);
+
+% Green element c
+c = [A(1,1) A(1,3) A(3,1) A(3,3)];
+
+% Display results
+disp('Vector v:')
+disp(v)
+
+disp('Matrix A:')
+disp(A)
+
+disp('a =')
+disp(a)
+
+disp('b =')
+disp(b)
+
+disp('c =')
+disp(c)
